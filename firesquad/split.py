a7d33ed8a18674ea9328ac5d0ef103e7b4f7cb4e
Carl Sverre <carl@carlsverre.com>
2013-06-04 22:21:50 -0700

    feature missing in split command:
    feature: actually working

--- firesquad/split.py
+++ firesquad/split.py
@@ -3,10 +3,15 @@ import os
 import math
 import itertools
 import time
+import sys
 
 class Split():
     def __init__(self, options):
         self.options = options
+        if self.options.chunk_size % 2 != 0:
+            print "--chunk-size must be an even number of bytes!"
+            sys.exit(1)
+
         self.split()
 
     def split(self):
@@ -32,16 +37,16 @@ class Split():
     def split_file(self, filename, output_dir):
         prefix_gen = itertools.imap(lambda i: "%03d" % i, itertools.count(0))
         file_size = os.path.getsize(filename)
-        continue_working = True
+        read_all = False
         with open(filename, 'r') as fd:
-            while continue_working:
+            while not read_all:
                 # store the current position
                 b_start = fd.tell()
                 # seek chunk_size bytes forward
                 fd.seek(self.options.chunk_size, 1)
                 # if we have passed the end of the file, lets seek back to the end
                 if fd.tell() > file_size:
-                    continue_working = False
+                    read_all = True
                     fd.seek(0, 2)
 
                 # read until the next newline
@@ -51,28 +56,27 @@ class Split():
 
                 # and then doooooo it
                 output_filename = os.path.join(output_dir, prefix_gen.next())
-                self.procs[output_filename] = self.dd_split(filename, b_start, b_end, output_filename)
+                self.procs[output_filename] = self.dd_split(filename, b_start, b_end, output_filename, read_all)
 
                 while len([p for p in self.procs.values() if p.poll() is None]) > 16:
                     print "waiting for workers"
                     time.sleep(.1)
 
-    def dd_split(self, filename, b_start, b_end, output_filename):
-        count, blocksize = self.factors(b_end - b_start)
-        proc = subprocess.Popen(['dd', 'count=%d' % count, 'bs=%d' % blocksize, 'if=%s' % filename, 'of=%s' % output_filename],
-            stdin=None, stdout=None, stderr=subprocess.PIPE)
+    def dd_split(self, filename, b_start, b_end, output_filename, read_all):
+        if read_all:
+            skip_count = b_start
+            blocksize = 1
+            count = b_end - b_start
+        else:
+            blocksize = self.options.chunk_size
+            skip_count = b_start / blocksize
+            count = 1
+        proc = subprocess.Popen([
+            'dd',
+            'skip=%d' % skip_count,     # skip <skip_count> blocks
+            'count=%d' % count,         # read 1 block
+            'bs=%d' % blocksize,        # each block is <bs> size in bytes
+            'if=%s' % filename,         # read from <if>
+            'of=%s' % output_filename   # write to <of>
+        ], stdin=None, stdout=None, stderr=subprocess.PIPE)
         return proc
-
-    def factors(self, n):
-        if n % 2 == 0:
-            # special case 2 so we can remove all even numbers from the search
-            return 2, n / 2
-
-        i = int(math.sqrt(n))
-        for j in range(3, i + 1, 2):
-            if n % j == 0:
-                # at this point j is the smallest divisor so lets return the inverse
-                return j, n / j
-        # n is probably prime or some bullshit like that
-        return 1, n
-
